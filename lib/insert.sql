9b9b9fd48e8c8b70565e27b89235cb20a80b8342
Lester Young <lyoung070789@gmail.com>
2020-03-10 18:46:14 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO projects (title, category, funding_goal, start_date. end_date) VALUES ("Water", "Environment", 03/10/2020, 04/10/2020);
+-- INSERT INTO projects (title, category, funding_goal, start_date. end_date) VALUES ("Water", "Environment", 03/10/2020, 04/10/2020);
