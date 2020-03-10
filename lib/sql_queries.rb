c10d51100815e454565680a4931764f8425d66cb
Lester Young <lyoung070789@gmail.com>
2020-03-10 19:13:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.name, pledge.amount
   FROM projects
-  JOIN pledge
+  JOIN pledges
   ON project_id = pledges.project_id;"
 end
 
