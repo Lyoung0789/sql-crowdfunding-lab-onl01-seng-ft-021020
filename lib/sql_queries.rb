86a6c014df2e8cd4df3ebc621054250678df7fbd
Lester Young <lyoung070789@gmail.com>
2020-03-10 19:43:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,6 +29,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects 
   JOIN pledges
   ON projects.id = pledges.project_id
+  WHERE SUM(pledges.amount) >= prjects.funding_goal
   GROUP BY projects.title;"
 end
 
